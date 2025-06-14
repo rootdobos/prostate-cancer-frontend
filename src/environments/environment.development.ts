6380469e8dccdbad80c51f6cee87adb881bcc149
rootdobos <49080519+rootdobos@users.noreply.github.com>
2025-06-15 01:28:31 +0300

    modified readme

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,4 @@
 export const environment = {
-  slides_folder: 'F:/storage/data-source',
-
   images_url: 'http://127.0.0.1:8000/images',
   placeholder_image: 'http://127.0.0.1:8000/images/placeholder.jpg',
   backend_url: 'http://127.0.0.1:8000',
